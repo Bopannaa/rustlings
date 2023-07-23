36aff10f4598439e45c933becc47db101d3f26db
Bopanna <bopannaa@gmail.com>
2023-07-23 23:16:47 +0530

    completed 61 exercises

--- exercises/traits/traits4.rs
+++ exercises/traits/traits4.rs
@@ -9,6 +9,8 @@
 
 // I AM NOT DONE
 
+use std::ffi::VaList;
+
 pub trait Licensed {
     fn licensing_info(&self) -> String {
         "some information".to_string()
@@ -23,7 +25,7 @@ impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn compare_license_types(software: ??, software_two: ??) -> bool {
+fn compare_license_types(software: impl Licensed, software_two: impl Licensed) -> bool {
     software.licensing_info() == software_two.licensing_info()
 }
 
